dc0402644b32bc31d91ea4102178e98ba268c93e
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-27 18:41:13 +0500

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -61,9 +61,6 @@ public class ArrayTasks {
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == number)
                 return i;
-            else
-                return -1;
-
         }
         return -1;
     }
